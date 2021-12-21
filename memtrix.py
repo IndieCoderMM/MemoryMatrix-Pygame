37043f1c6eb28459ab1b5a12ef4e529450de324a
heinthantchk <hthant00ckh@gmail.com>
2021-12-21 22:27:50 +0630

    v1.1 completed

--- memtrix.py
+++ memtrix.py
@@ -7,7 +7,7 @@ HEIGHT = 500
 FONTSIZE = 20
 SHOWTIME = 500
 SHRINKAGE = 7
-TIMER = 30
+TIMER = 10
 FPS = 30
 
 WHITE = (255, 255, 255)
@@ -15,91 +15,82 @@ RED = (223, 32, 41)
 YELLOW = (245, 242, 10)
 BLUE = (0, 38, 97)
 GREY = (96, 125, 139)
-CYAN = (0, 188, 212)
 LIME = (205, 220, 57)
-
-
 
 pygame.init()
 clock = pygame.time.Clock()
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Memtrix')
-icon = pygame.image.load('icon1.png')
+icon = pygame.image.load('icon.png')
 pygame.display.set_icon(icon)
 
 normal_font = pygame.font.Font('freesansbold.ttf', FONTSIZE)
-big_font = pygame.font.Font('freesansbold.ttf', FONTSIZE+10)
+big_font = pygame.font.Font('freesansbold.ttf', FONTSIZE+30)
 
-LEVEL = 3
-TILESIZE = 100
+timestamp = {'past':0, 'now':0, 'timer':0}
+timer_paused = True
+timer_up =	False
 
+level = 3
+tile_size = 100
 new_pattern = []
 input_pattern = []
 score = 0
 hi_score = 0
-timestamp = {'past':0, 'now':0, 'timer':0}
-
-timer_paused = True
-timer_up =	False
-
-
 
 def getLeftTop(row, col):
-	left = (TILESIZE * col) + (WIDTH - TILESIZE * LEVEL) // 2
-	top = (TILESIZE * row) + (HEIGHT - TILESIZE * LEVEL) // 2
+	board_size = tile_size * level
+	left = (tile_size * col) + (WIDTH - board_size) // 2
+	top = (tile_size * row) + (HEIGHT - board_size) // 2
 	return left, top
 
 def getClickedTile(x, y):
-	for row in range(LEVEL):
-		for col in range(LEVEL):
+	for row in range(level):
+		for col in range(level):
 			left, top = getLeftTop(row, col)
-			area = pygame.Rect(left, top, TILESIZE, TILESIZE)
+			area = pygame.Rect(left, top, tile_size, tile_size)
 			if area.collidepoint(x, y):
 				return row, col 
 	return None, None
 
-def drawText(msg, x, y, large):
-	if large:
-		font = big_font
-	else:
-		font = normal_font
+def drawText(msg, x, y, large=False):
+	font = big_font if large else normal_font
 	text = font.render(msg, True, WHITE)
 	rect = text.get_rect()
+	if x == 'center':
+		x = int(WIDTH/2 - text.get_width()/2)
 	rect.topleft = (x, y)
 	win.blit(text, rect)
 
-def displayScore():
-	drawText(f'Score: {score}', 10, 10, False)
-	drawText(f'Best: {hi_score}', WIDTH-100, 10, False)
-
 def drawBoard():
 	win.fill(BLUE)
-	displayScore()
+	drawText(f'Score: {score}', 20, 10)
+	drawText(f'Best: {hi_score}', WIDTH-100, 10)
 	# Drawing Table
-	for row in range(LEVEL):
-		for col in range(LEVEL):
+	for row in range(level):
+		for col in range(level):
 			left, top = getLeftTop(row, col)
-			pygame.draw.rect(win, GREY, (left, top, TILESIZE-3, TILESIZE-3))
+			pygame.draw.rect(win, GREY, (left, top, tile_size - 3, tile_size - 3))
 	# Drawing border
+	board_size = tile_size * level
 	left, top = getLeftTop(0, 0)
-	pygame.draw.rect(win, RED, (left-5, top-5, TILESIZE*LEVEL+6, TILESIZE*LEVEL+6), 4)
-
+	pygame.draw.rect(win, RED, (left-5, top-5, board_size+6, board_size+6), 4)
 	# Drawing timer
-	pygame.draw.rect(win, LIME, (0, HEIGHT-8, int(WIDTH*(1-(timestamp['timer']/TIMER))), 8))
-
+	time_ratio = timestamp['timer'] / TIMER		# e.g. if 30 / 60, length will be half of screen
+	pygame.draw.rect(win, LIME, (0, HEIGHT-8, int(WIDTH*(1-time_ratio)), 8))
 
 def generatePattern():
 	pattern = []
-	while len(pattern) < LEVEL:
-		rand_row = random.randrange(LEVEL)
-		rand_col = random.randrange(LEVEL)
-		if (rand_row, rand_col) not in pattern:
-			pattern.append((rand_row, rand_col))
+	while len(pattern) < level:
+		rand_row = random.randrange(level)
+		rand_col = random.randrange(level)
+		if (rand_row, rand_col) in pattern:	continue
+		pattern.append((rand_row, rand_col))
 	return pattern 
 
 def lightUp(row, col, color):
 	left, top = getLeftTop(row, col)
-	surface = pygame.Surface((TILESIZE-3, TILESIZE-3))
+	surface = pygame.Surface((tile_size - 3, tile_size - 3))
 	surface.fill(color)
 	win.blit(surface, (left, top))
 
@@ -110,27 +101,42 @@ def startTimer():
 		timestamp['timer'] += (timestamp['now'] - timestamp['past'])
 		timestamp['past'] = timestamp['now']
 
+def gameOver():
+	win.fill(RED)
+	drawText("Time's Up!", 'center', 50, True)
+	drawText(f'Score: {score}', 'center', 150, True)
+	drawText(f'Best: {hi_score}', 'center', 250, True)
+	drawText('Press SPACE to play again', 'center', 350)
+	pygame.display.update()
+
+def startScreen():
+	win.fill(BLUE)
+	drawText('Memory Matrix', 'center', HEIGHT//2, True)
+	drawText("Press SPACE to start", 'center', 350)
+	pygame.display.update()
+
 running = True
+started = False
 waiting_input = False
 
-while running:
-	startTimer()
-	drawBoard()
-	
 
+# Main Loop
+while running:
+	# Checking inputs
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
-		# Restart if game is over
+		# Restart if times up
 		if event.type == pygame.KEYUP:
 			if event.key == pygame.K_SPACE:
+				if not started: started = True
 				if timer_up:
 					timer_up = False
 					waiting_input = False
 					score = 0
-					LEVEL = 3
-					TILESIZE = 100
-					timestamp = {'past':0, 'now':0, 'timer':0}
+					level = 3
+					tile_size = 100
+					timestamp = {key:0 for key in timestamp.keys()}
 					new_pattern = []
 					input_pattern = []
 					drawBoard()
@@ -142,17 +148,26 @@ while running:
 				if (row, col) not in input_pattern:
 					input_pattern.append((row, col))
 
-	if timestamp['timer'] > TIMER:	timer_up = True
+	if not started:
+		startScreen()
+		continue
+
+	startTimer()
+	drawBoard()
+
+	if timestamp['timer'] > TIMER:
+		timer_up = True
+		gameOver()
 
 	if waiting_input and not timer_up:
-		# Display tiles whether right or wrong
+		# Displaying tiles with color according to right/wrong
 		for (row, col) in input_pattern:
 			if (row, col) in new_pattern:
 				lightUp(row, col, YELLOW)
 			else:
 				lightUp(row, col, RED)
 		pygame.display.update()
-
+		# Checking numbers of input
 		if len(input_pattern) == len(new_pattern):
 			waiting_input = False
 			timer_paused = True
@@ -161,26 +176,27 @@ while running:
 				lightUp(row, col, YELLOW)
 			pygame.display.update()
 			pygame.time.wait(SHOWTIME)
-			# Check if patterns are match
+			# Checking patterns
 			if sorted(input_pattern) == sorted(new_pattern):
-				score += (LEVEL - 2)
+				score += (level - 2)
 				if score > hi_score:
-					hi_score = score 
-
-				if LEVEL < 6:
-					LEVEL += 1
-					TILESIZE = int(TILESIZE - TILESIZE / SHRINKAGE) 
+					hi_score = score
+				if level < 6:
+					level += 1
+					tile_size = int(tile_size - tile_size / SHRINKAGE)
+			# Back to previous level if wrong pattern
 			else:
-				if LEVEL > 3:
-					LEVEL -= 1
-					TILESIZE = int(TILESIZE + TILESIZE / SHRINKAGE)
+				score -= 1
+				if level > 3:
+					level -= 1
+					tile_size = int(tile_size + tile_size / SHRINKAGE)
 
+	# if not waiting then display new pattern
 	elif not waiting_input and not timer_up:
-		waiting_input = True
 		timer_paused = True
 		new_pattern = []
 		input_pattern = []
-
+		# Displaying empty board
 		pygame.display.update()
 		pygame.time.wait(SHOWTIME)
 		# Display the new pattern 
@@ -189,19 +205,12 @@ while running:
 			lightUp(row, col, YELLOW)
 		pygame.display.update()
 		pygame.time.wait(SHOWTIME)
-		
+
+		waiting_input = True
 		timer_paused = False
 		timestamp['past'] = time.time()
-
-		# 	if LEVEL > 3:
-		# 		LEVEL -= 1
-		
-		# timer_paused = False
-		# timestamp['past'] = time.time()
 
 	pygame.display.update()
 	clock.tick(FPS)
 
 pygame.quit()
-
-
